87114739865cc6697928b5075f7a3492bca76aed
Carlos Eduardo <ce83413@gmail.com>
2024-09-11 21:15:07 -0300

    Service layer, component registration

--- src/main/java/com/aprendendoSpringBoot/course/entities/User.java
+++ src/main/java/com/aprendendoSpringBoot/course/entities/User.java
@@ -11,12 +11,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = ("tb_user"))
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)		//definição de autoincrement
 	private Long id; //@Id define o id como chave primária
 	private String name;
 	private String email;
